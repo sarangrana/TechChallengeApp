1cd9e5dc67119f4a9a0391f0a004926e857b3090
Sarang Rana <sarangrana@gmail.com>
2022-02-05 16:23:25 +0530

    Adding terraform files to create AWS infra

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,5 @@
+project_name = "servian"
+environment = "dev"
+vpc_name =  "vpc-servian"
+servian_vpc_cidr = "10.0.0.0/16"
+servian_subnets_cidr = ["10.0.1.0/24", "10.0.2.0/24"]
---
+++ terraform/variables.tf
@@ -0,0 +1,53 @@
+variable "aws_access_key" {
+  description = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+}
+
+variable "project_name" {
+  description = "To differentiate the resources based on project name"
+  default     = "servian-tech-challenge"
+}
+
+variable "environment" {
+  description = "To decide project environment"
+  default     = "dev"
+}
+
+variable "vpc_name" {
+  description = "New VPC name"
+  default     = "vpc-servian"
+}
+
+variable "servian_vpc_cidr" {
+  description = "Servian VPC cidr"
+  default     = "10.0.0.0/16"
+}
+
+variable "servian_subnets_cidr" {
+	type = list
+	default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "azs" {
+	type = list
+	default = ["us-east-2a", "us-east-2b"]
+}
+
+variable "postgre_db_version" {
+  description = "PostgreSQL Database version to be used"
+  default     = "10.07"
+}
+
+variable "postgre_db_password" {
+  description = "PostgreSQL Database password"
+  default     = "12345678"
+  sensitive   = true
+}
+
+variable "postgresql_instance_type" {
+  description = "PostgreSQL Database instance type"
+  default     = "db.t3.medium"
+}
